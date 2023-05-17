0c6d859961ca27280a77281c5afe2795eade9500
Jesús Aransay <jmaransay@gmail.com>
2023-05-18 01:18:25 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# icd_sesion07
+A Vagrant virtual machine with Docker, Jenkins and portainer.io installed
---
+++ jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Instalamos jdk11
+
+apt-get update
+apt-get -y install openjdk-11-jre
+
+#  Descargamos la clave pública del repositorio y la almacenamos
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc
+
+# Creamos el repositorio para Debian de Jenkins
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+
+# Actualizamos repositorios
+apt-get update
+
+# Lo instalamos, junto a otros paquetes necesarios:
+apt-get -y install jenkins
+
+# Lo dejamos preparado para que arranque en inicio por si reseteamos
+# systemctl enable jenkins
